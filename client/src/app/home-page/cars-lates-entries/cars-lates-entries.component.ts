b9e462ae3fb740ec8a0e9f59e1e5c5f1a611fd77
YordanDochev <yordan.dochev13@gmail.com>
2024-03-20 16:06:14 +0200

    Home page template separate by components

---
+++ client/src/app/home-page/cars-lates-entries/cars-lates-entries.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cars-lates-entries',
+  templateUrl: './cars-lates-entries.component.html',
+  styleUrls: ['./cars-lates-entries.component.css']
+})
+export class CarsLatesEntriesComponent {
+
+}
--- client/src/app/home-page/home-page.module.ts
+++ client/src/app/home-page/home-page.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { BannerComponent } from './banner/banner.component';
 import { HomePageComponent } from './home-page.component';
 import { SharedModule } from '../shared/shared.module';
+import { RequestInfoComponent } from './request-info/request-info.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CarsLatesEntriesComponent } from './cars-lates-entries/cars-lates-entries.component';
 
 @NgModule({
-  declarations: [BannerComponent, HomePageComponent],
+  declarations: [BannerComponent, HomePageComponent, RequestInfoComponent, AboutUsComponent, CarsLatesEntriesComponent],
   imports: [CommonModule,SharedModule],
   exports: [HomePageComponent],
 })
