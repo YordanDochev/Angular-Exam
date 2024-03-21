1f7a65674f6d117de30a54d47651c11153046a24
YordanDochev <yordan.dochev13@gmail.com>
2024-03-21 02:26:16 +0200

    Register and Login page has been separate by components

--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FormLoginComponent } from './login/form-login/form-login.component';
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent],
+  declarations: [LoginComponent, RegisterComponent, FormLoginComponent ],
   imports: [CommonModule],
   exports: [LoginComponent,RegisterComponent],
 })
